bf5787a5d95d0c1d8e99ff680814ee201c4e4b28
AVINASH KUMAR <Aviproo>
2023-09-19 16:53:00 +0530

    axios geting data from crud and showing

--- crudcrud/crud.js
+++ crudcrud/crud.js
@@ -24,3 +24,18 @@ function ButtonClicked() {
       main.innerHTML = `<h4>Something went wrong</h4>`;
     });
 }
+axios
+  .get(
+    "https://crudcrud.com/api/ddcd6265ab1847c4848569071113d31e/AppointmentData"
+  )
+  .then((res) => {
+    let resdata = "";
+    console.log(res);
+    for (let i = 0; i < res.data.length; i++) {
+      resdata += `<div>Name:${res.data[i].name}-email:${res.data[i].email}-number:${res.data[i].number}<button>Delete</button> <button>Edit</button> </div>`;
+    }
+    main.innerHTML = resdata;
+  })
+  .catch((rej) => {
+    main.innerHTML = `<h4>Something went wrong</h4>`;
+  });
